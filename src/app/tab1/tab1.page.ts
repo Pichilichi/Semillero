02cdfb799d5f814f91952ddd5fed81cb3004d15f
Francisco García Macías <garmacfco@gmail.com>
2021-04-18 19:47:17 +0200

    Opiniones v2
    Ya se pueden ver todas las opiniones y filtrarlas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,37 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  data: any;
+  opiniones = {};
+  id: number;
+
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.data = '';
+    this.authService.getOpiniones(this.authService.token).then(data => {
+      this.opiniones = data
+      this.data = this.filtroEnorme(data)
+      //console.log(this.data)
+    });
+  } 
+
+  change(){
+    this.authService.getOpiniones(this.authService.token).then(data => {
+      this.opiniones = data
+      this.data = this.filtroPlaga(data, this.id)
+    });
+  }
+
+  filtroPlaga(toSort: any, id){
+    if(id == 11){
+      return toSort.data.filter((element) => element.id < 100)
+    }
+    else{
+    return toSort.data.filter((element) => element.plague_id == id)
+    }
+  }
+
+  filtroEnorme(toSort: any){
+    return toSort.data.filter((element) => element.id < 100)
+  }
 
 }
