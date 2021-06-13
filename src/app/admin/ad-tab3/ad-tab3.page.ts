a42532ca368ea3b6354a8f2c6cd97574dff07a3d
Francisco García Macías <garmacfco@gmail.com>
2021-06-13 20:39:12 +0200

    Graficas v2
    Algunos ajustes

--- src/app/admin/ad-tab3/ad-tab3.page.ts
+++ src/app/admin/ad-tab3/ad-tab3.page.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Chart, ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
+import { AuthService } from 'src/app/services/auth.service';
 
 
 @Component({
@@ -10,21 +12,111 @@ import { Label } from 'ng2-charts';
 })
 export class AdTab3Page implements OnInit {
 
-  constructor() { }
+  data: any;
+  opiniones = {};
+  PlagasNombres = [];
+  OpinionesPlagas = [];
+  plagas = [];
 
-  ngOnInit() {
-  }
-
+  ctx = document.getElementById("myChart")
   barChartOptions: ChartOptions = {
     responsive: true,
   };
-  barChartLabels: Label[] = ['Plataformas','Jugadores','Juegos','Abedules','Avellanos'];
+  
+  barChartLabels: Label[] = ['Enero','Febrero','Marzo','Abril','Mayo','Junio'];
   barChartType: ChartType = 'bar';
   barChartLegend = true;
   barChartPlugins = [];
 
-  barChartData: ChartDataSets[] = [
-    {data: [40,35,46,2,89,60], label: 'Falta Calle'}
-  ];
+
+  barChartData: ChartDataSets[];
+
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.data = '';
+    this.authService.getOpiniones(this.authService.token).then(data =>{
+      this.opiniones = data;
+      this.data = this.filtroOpiniones(data)
+      //console.log(this.data)
+      //console.log(this.data[0].plague_name)
+      for(let i = 0; i < this.data.length; i++){
+        this.OpinionesPlagas[i] = this.data[i].plague_name;
+      }
+      //console.log(this.OpinionesPlagas);
+    });
+    this.authService.getPlagas(this.authService.token).then(data => {
+      this.plagas = this.filtroTodasPlagas(data);
+      //console.log(this.plagas)
+      for(let i = 0; i < this.plagas.length; i++){
+        this.PlagasNombres[i] = this.plagas[i].name;
+      }
+      console.log(this.PlagasNombres[6]);
+
+      
+    });
+
+    this.barChartData = [
+      {data: [0,1,2,3,4,5], label: 'Dwight'},
+    ];
+
+    // this.barChartData = [
+    //   {data: [0,0,0,0,0,0], label: 'Dwight'},
+    //   {data: [1,1,1,1,1,1], label: 'Kacey'},
+    //   {data: [2,2,2,2,2,2], label: 'Insectos'},
+    //   {data: [3,3,3,3,3,3], label: 'Kirk'},
+    //   {data: [4,4,4,4,4,4], label: "Anais"},
+    //   {data: [5,5,5,5,5,5], label: "Deshaun"},
+    //   {data: [6,6,6,6,6,6], label: "Enos"},
+    //   {data: [7,7,7,7,7,7], label: 'Breana'},
+    //   {data: [8,8,8,8,8,8], label: "Boyle"},
+    //   {data: [9,9,9,9,9,9], label: this.PlagasNombres[9]},
+    // ];
+    
   
+  
+   }
+
+  ngOnInit() {
+  }
+
+ 
+  filtroOpiniones(toSort: any){
+    return toSort.data.filter((element) => element.id < 100)
+  }
+
+  filtroTodasPlagas(toSort: any){
+    return toSort.data.filter((element) => element.id < 100);
+  }
+
+  // getNombre(i: number){
+  //   // console.log(this.PlagasNombres)
+  //    console.log(this.PlagasNombres[1])
+  //   return this.PlagasNombres[i];
+  // }
+  
+  cambio(id){
+    this.removeData;
+    const labelsNew = [
+      this.PlagasNombres[id]
+    ]
+    const datos = [
+      2
+    ]
+    this.addData(Chart,datos,labelsNew);
+  }
+
+  
+  dataNew={}
+  removeData(chart) {
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+  }
+
+  addData(chart, data, id) {
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
 }
